1dd4d743a2a0a7475791b1d5f0fa3d62276223a1
myrayzsiza <rayzanitah2@gmail.com>
2026-09-15 12:50:30 +0300

    Sets and Maps

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,17 @@
+using System.Collections.Generic;
+
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    public List<Feature> Features { get; set; }
+}
+
+public class Feature
+{
+    public FeatureProperties Properties { get; set; }
+}
+
+public class FeatureProperties
+{
+    public string Place { get; set; }
+    public double? Mag { get; set; }
 }
